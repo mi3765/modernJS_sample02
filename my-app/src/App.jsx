87fd375e5d172e506eff558e2b7f4ab90aebcb0c
mi3765 <chips1225@outlook.jp>
2023-06-11 17:53:35 +0900

    ColorfulMessage.jsxを追加

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ColorfulMessage from './components/ColorfulMessage';
 
 const App = () => {
+    const [num, setNum] = useState(0);
+    const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+    const onClickCountUp = () => {
+        setNum(num + 1);
+    };
+
+    const onClickSwitchShowFlag = () => {
+        setFaceShowFlag(!faceShowFlag);
+    };
+
     return (
         <>
-            <h1>こんにちは！</h1>
-            <p>お元気ですか？</p>
+            <h1 style={{ color: 'red' }}>こんにちは！</h1>
+            <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+            <ColorfulMessage color="pink">元気です！</ColorfulMessage>
+            <button onClick={onClickCountUp}>カウントアップ!</button>
+            <br></br>
+            <button onClick={onClickSwitchShowFlag}>on/off</button>
+            <p>{num}</p>
+            { faceShowFlag && <p>( ;∀;)</p> }
         </>
     );
 };
